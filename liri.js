17ab56b6dcae08284f257292a380c562a306d7fa
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-05-20 21:43:47 -0700

    liri main files, input gatherer and key link added

--- liri.js
+++ liri.js
@@ -3,4 +3,19 @@ require("dotenv").config();
 var twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
-var fs(require fs);
+var fs = require (fs);
+
+var keys = require('./keys.js');
+
+var node = process.argv;
+
+var  command = "";
+
+for(var i=2; i<node.length;i++){
+    if(i<command.length){
+        command += "+" + node[i];
+    }else{
+        command += node[i];
+    }
+}
+
