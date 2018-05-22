3b0a8902491e34408c57a11aaa4a2d71407fb10c
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-05-21 18:09:52 -0700

    Attempt to append to text to file and fix program

--- liri.js
+++ liri.js
@@ -1,41 +1,72 @@
-require("dotenv").config();
-
-var twitter = require('twitter');
-var spotify = require('spotify');
-var request = require('request');
-var fs = require (fs);
+var program = require('dotenv').config()
 
 var keys = require('./keys.js');
 
+var Twitter = require('twitter');
+var spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require ('fs');
+
+
 var node = process.argv;
 
-var  command = "";
+var command = node[2];
 
-for(var i=2; i<node.length;i++){
+var stuffToDo = "";
+
+for(var i=3; i<node.length;i++){
     if(i<command.length){
-        command += "+" + node[i];
-    }else{
-        command += node[i];
+        stuffToDo += node[i] + " ";
     }
 }
 
 function socialApp(){
-    var user = new twitter(keys.twitterKeys);
+    fs.appendFile('./log.txt','Command: node liri.js my-tweets\n', function(error){
+        if(error){
+            throw error;
+        }
+    })
+
+    var client = new Twitter(keys.twitterKeys);
 
     var account = {screen_name: 'realDonaldTrump', count: 20};
     
     var output = "**********\n" + "Trump's Tweets:\n" + "**********\n\n";
 
-    user.get('statuses/user_timeline', account, function(error, tweets, response){
-        if(!error && response.statusCode === 200){
+    client.get('statuses/user_timeline', account, function(error, tweets, response){
+        if(error){
+            var errorText = "Twitter Error";
+            fs.appendFile('./log.txt',errorText, function(error){
+                if(error){
+                    throw error;
+                    console.log(errorText);
+                }
+            })
+            return
+        } else {
+
+            var output = "**********\n" + "Trump's Tweets:\n" + "**********\n\n";
+
             for(var i=0; i < tweets.length; i++){
                 output += "'" + tweets[i].text + "' " + "\n" + tweets[i].created_at + "\n" + "**********\n";
             }
+
+            fs.appendFile("./log.txt", "LIRI:\n" + output + "\n", function(error){
+                if(errror){
+                    throw error;
+                }
+                console.log(output);
+            })
         }
     })
 }
 
 function musicApp(song){
+    fs.appendFile("./log.txt", "Command: node liri.js spotify-this-song" + song + "\n", function(error){
+        if(error){
+            throw error;
+        }
+    })
     var search;
     if(song === ""){
         search = "Here on Earth Tiesto";
@@ -44,14 +75,46 @@ function musicApp(song){
     }
 
     spotify.search({type: 'track', query: search}, function(error, data){
-        if(!error){
+        if(error){
+            var errorText1 = "Spotify Error";
+
+            fs.appendFile(".log/txt", errorText1, function(error){
+                if(errror){
+                    throw error
+                    console.log(errorText1);
+                }
+            })
+            return
+        }else{
             var info = data.tracks.items[0];
-            var output = "**********\n" + "Song:\n" + "**********\n\n" + "Name: " + info.name + "\n" + "Artist: " + info.artists[0].name + "\n" + "Album: " + info.album.name + "\n" + "Preview: " + info.preview_url + "\n";
+            if(!info){
+                var errorText2 = "Spotify Error 2";
+                fs.appendFile("./log.txt", errorText2, function(error){
+                    if(error){
+                        throw error;
+                        console.log(errorText2);
+                    }
+                })
+                return;
+            }else{
+                var output = "**********\n" + "Song:\n" + "**********\n\n" + "Name: " + info.name + "\n" + "Artist: " + info.artists[0].name + "\n" + "Album: " + info.album.name + "\n" + "Preview: " + info.preview_url + "\n";
+                fs.appendFile("./log.txt", "LIRI:\n" + output + "\n", function(error){
+                if(errror){
+                    throw error;
+                }
+                console.log(output);
+                })
+            }
         }
     })
 }
 
 function movieApp(movie){
+    fs.appendFile("./log.txt", "Command: node liri.js movie-this " + movie + "\n", function(error){
+        if(error){
+            throw error;
+        }
+    })
     var search;
     if(movie === ""){
         search = "Fight Club";
@@ -63,32 +126,86 @@ function movieApp(movie){
     var queryUrl = "http://www.omdbapi.com/?t=" + search + "&plot=fullt&tomatoes=true&apikey=trilogy";
 
     request(queryUrl, function(error, response, body){
-        var info = JSON.parse(body);
-        var output = "**********\n" + "Movie:\n" + "**********\n\n" + "Title: " + info.Title + "\n" + "Release Date: " + info.Released + "\n" + "IMBD Rating: " + info.imbdRating + "\n" + "Country of Origin: " + info.Country + "\n" + "Language: " + info.Language + "\n" +  "Plot: " + info.Plot + "\n" + "Actors: " + info.Actors + "\n" + "Country of Rotten Tomatoes Rating: " + info.tomatoRating + "\n" + "Rotten Tomatoes URL: " + info.tomatoURL + "\n";
-
+        if(error || response.statusCode !== 200){
+            var errorText1 = "OMDB Error";
+            fs.appendFile(".log/txt", errorText1, function(error){
+                if(errror){
+                    throw error
+                    console.log(errorText1);
+                }
+            }) 
+            return;
+        }else{
+            var info = JSON.parse(body);
+            if(info.title && !info.Released && !info.imdbRating){
+                var errorText2 = "OMDB Error2";
+                fs.appendFile("./log.txt", errorText2, function(error){
+                    throw errorText2
+                    console.log(errorText2);
+                })
+            return;
+            }else{
+                var output = "**********\n" + "Movie:\n" + "**********\n\n" + "Title: " + info.Title + "\n" + "Release Date: " + info.Released + "\n" + "IMBD Rating: " + info.imbdRating + "\n" + "Country of Origin: " + info.Country + "\n" + "Language: " + info.Language + "\n" +  "Plot: " + info.Plot + "\n" + "Actors: " + info.Actors + "\n" + "Country of Rotten Tomatoes Rating: " + info.tomatoRating + "\n" + "Rotten Tomatoes URL: " + info.tomatoURL + "\n";
+                fs.appendFile("./log.txt", "LIRI:\n" + output + "\n", function(error){
+                if(errror){
+                    throw error;
+                }
+                console.log(output);
+                })
+            }
+        }
     })
 }
 
 function justDoIt(){
-    var command = info.split(",");
-    var action = command[0].trim();
-    var arguments = command[1].trim();
-    
-    if(action === "my-tweets"){
-        socialApp();
-    }else if(action === "spotify-this-song"){
-        musicApp(argument);
-    }else if(action === "movie-this"){
-        movieApp(argument);
-    }
+    fs.appendFile("./log.txt", "Command: liri.js do-what-it-says\n", function(error){
+        if(error){
+            throw error;
+        }
+    })
+    fs.readFile("./random.txt", "utf8", function(error, data){
+        if(error){
+            console.log("Error Reading File");
+            return;
+        }else{
+            var command = info.split(",");
+            var action = command[0].trim();
+            var parameters = command[1].trim();
+
+            switch(command){
+                case "my-tweets":
+                    socialApp();
+                    break;
+                case "spotify-this-song":
+                    musicApp(parameters);
+                    break;
+                case "movie-this":
+                    movieApp(parameters);
+                    break;
+            }
+        }
+    })   
 }
 
 if(command === "my-tweets"){
     socialApp();
 }else if(command === "spotify-this-song"){
-    musicApp();
+    musicApp(stuffToDo);
 }else if(command === "movie-this"){
-    movieApp();
+    movieApp(stuffToDo);
 }else if(command === "do-what-it-says"){
     justDoIt();
+}else{
+    fs.appendFile("./log.txt", "Command: " + node + "\n", function(error){
+        if(error){
+            throw error;
+        }
+        output = "History:\n" + " node liri.js my-tweets\n" + " node liri.js spotify-this-song "<song_name>\n"" + " node liri.js movie-this "<movie_name>\n"" + " node liri.js do-what-it-says\n";
+        fs.appendFile("./log.txt", "LIRI:\n" + output + "\n", function(error){
+            if(error){
+                throw error;
+            }
+            console.log(output);
+        })
+    })
 }
